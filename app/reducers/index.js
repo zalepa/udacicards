096c32af9728163c082ed1f20dbb030d5d2a3fb6
George Zalepa <george.zalepa@gmail.com>
2018-03-22 14:31:27 -0400

    Hook up NewDeck to Redux; add base NewDeck UI.

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -5,6 +5,7 @@ import { CREATE_DECK } from '../actions';
 function reducer(state = [], action) {
   switch (action.type) {
     case CREATE_DECK:
+      console.log([...state, action.deck])
       return [...state, action.deck];
     default:
       return state;
--- app/screens/NewDeck/index.js
+++ app/screens/NewDeck/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TextInput, Button} from 'react-native';
+import { createDeck } from '../../actions';
+import { connect } from 'react-redux';
 
-export default class NewDeck extends React.Component {
+class NewDeck extends React.Component {
+
+  state = {
+    text: ''
+  }
+
+  handleTitleChange = (text) => {
+    this.setState({text})
+  }
+
+  onCreateNewDeck = () => {
+    this.props.dispatch(createDeck(this.state));
+  }
+
   render() {
     return (<View>
-      <Text>New Deck</Text>
+      <Text>What is the title of your new deck?</Text>
+      <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={this.handleTitleChange}
+        value={this.state.text}
+      />
+      <Button
+        onPress={this.onCreateNewDeck}
+        title="Submit"
+        color="#841584"
+        accessibilityLabel="Create a new deck"
+      />
     </View>)
   }
 }
+
+export default connect()(NewDeck);
