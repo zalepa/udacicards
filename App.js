bd769cd780c85450aac37829a3265af7c7ed5311
George Zalepa <george.zalepa@gmail.com>
2018-03-22 14:22:11 -0400

    Add store to App

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { View } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './app/reducers';
 import Decks from './app/screens/Decks';
 import MainNavigation from './app/components/MainNavigation';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{flex: 1, justifyContent: "center", paddingTop: 20}}>
-        <MainNavigation />
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View style={{flex: 1, justifyContent: "center", paddingTop: 20}}>
+          <MainNavigation />
+        </View>
+      </Provider>
     );
   }
 }
