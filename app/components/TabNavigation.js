a763c85e1f074c1e682a3b8109b9a58229e7d12a
George Zalepa <george.zalepa@gmail.com>
2018-03-23 18:34:16 -0400

    Add icons to Tab navigator

--- app/components/TabNavigation.js
+++ app/components/TabNavigation.js
@@ -1,16 +1,22 @@
+import React from 'react';
 import Decks from '../screens/Decks';
 import NewDeck from '../screens/NewDeck';
 import { TabNavigator } from 'react-navigation';
+import { Ionicons, MaterialCommunityIcons} from '@expo/vector-icons';
 
 const TabNavigation = TabNavigator({
   Home: {
     screen: Decks,
     navigationOptions: {
-      title: 'Decks'
+      title: 'Decks',
+      tabBarIcon: () => <Ionicons name='ios-list-outline' size={30}/>
     }
   },
   New: {
     screen: NewDeck,
+    navigationOptions: {
+      tabBarIcon: () => <Ionicons name='ios-add' size={30}/>
+    }
   }
 })
 
