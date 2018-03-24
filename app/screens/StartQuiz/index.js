8268748aa75e8e019744da75d257de590917cf95
George Zalepa <george.zalepa@gmail.com>
2018-03-23 22:35:28 -0400

    Style Quiz view

--- app/screens/StartQuiz/index.js
+++ app/screens/StartQuiz/index.js
@@ -1,6 +1,33 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {TouchableOpacity, Text, View, StyleSheet} from 'react-native';
 import { connect } from 'react-redux';
+
+const styles = StyleSheet.create({
+  status: {
+    fontSize: 15
+  },
+  content: {
+    fontSize: 30,
+    textAlign: 'center',
+    paddingTop: 30,
+    paddingBottom: 30,
+  },
+  button: {
+    backgroundColor: 'tomato',
+    margin: 5,
+    borderRadius: 10,
+    padding: 5,
+  },
+  buttonCorrect: {
+    backgroundColor: '#6DFF83'
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 19,
+    padding: 7,
+    textAlign: 'center'
+  }
+})
 
 class StartQuiz extends React.Component {
 
@@ -44,18 +71,25 @@ class StartQuiz extends React.Component {
 
   render() {
     return (<View>
-      <Text>{this.state.currentCardNumber} / {this.props.cards.length} ({(this.state.correctAnswers / this.props.cards.length) * 100}% correct)</Text>
-      <Text>
+      <Text style={styles.status}>{this.state.currentCardNumber} / {this.props.cards.length} ({(this.state.correctAnswers / this.props.cards.length) * 100}% correct)</Text>
+      <Text style={styles.content}>
         {this.state.viewState === 'question'
           ? this.props.cards[this.state.currentCardNumber].question
           : this.props.cards[this.state.currentCardNumber].answer
         }
       </Text>
 
-      <Button title={this.state.viewState === 'question' ? 'View Answer' : 'View Question'} onPress={this.toggleViewState} />
+      <TouchableOpacity style={[styles.button, {backgroundColor: '#FAFAFA'}]} onPress={this.toggleViewState}>
+        <Text style={[styles.buttonText, { color: 'darkgray'}]}>{this.state.viewState === 'question' ? 'View Answer' : 'View Question'}</Text>
+      </TouchableOpacity>
 
-      <Button title='Correct' onPress={this.onCorrectAnswer} style={{backgroundColor: 'green'}} />
-      <Button title='Inorrect' onPress={this.moveQuestion} />
+      <TouchableOpacity style={[styles.button,  styles.buttonCorrect]}  onPress={this.onCorrectAnswer}>
+        <Text style={styles.buttonText}>Correct</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={[styles.button]}  onPress={this.moveQuestion}>
+        <Text style={styles.buttonText}>Incorrect</Text>
+      </TouchableOpacity>
 
     </View>)
   }
