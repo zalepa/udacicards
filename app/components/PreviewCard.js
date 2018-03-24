ee8475945f67e324b14ec821950e2ba609ae8ff9
George Zalepa <george.zalepa@gmail.com>
2018-03-23 22:19:56 -0400

    Style index view

--- app/components/PreviewCard.js
+++ app/components/PreviewCard.js
@@ -6,34 +6,41 @@ const PreviewCard = ({deck, onPress}) => {
   return (
     <TouchableOpacity onPress={() => onPress(deck)}>
       <View style={styles.card}>
-      <Text style={styles.header}>
-        <MaterialCommunityIcons style={styles.icon} name='cards-variant' size={25}/>
-        <Text>{deck.title}</Text>
-      </Text>
-      <Text style={styles.size}>{deck.cards.length} cards</Text>
-    </View>
+        <Text style={styles.header}>
+          <MaterialCommunityIcons style={styles.icon} name='cards-variant' size={25}/>
+          <Text> {deck.title}</Text>
+        </Text>
+        <Text style={styles.size}>({deck.cards.length} cards)</Text>
+      </View>
     </TouchableOpacity>
   )
 }
 
 const styles = StyleSheet.create({
   card: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     borderColor: '#f0f0f0',
     borderWidth: 1,
     borderRadius: 10,
     padding: 10,
+    paddingLeft: 10,
     margin: 10,
   },
   header: {
     fontSize: 25,
-    textAlign: 'center',
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center'
   },
+  icon: {
+    width: 100
+  },
   size: {
-    textAlign: 'center'
+    textAlign: 'center',
+    fontSize: 15,
+    fontStyle: 'italic'
   }
 });
 
