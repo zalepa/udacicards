a04589d80c6fbdc50fdf163ef69491d44c2877f8
George Zalepa <george.zalepa@gmail.com>
2018-03-24 13:18:10 -0400

    Add validation alerts and success alerts to AddCard.

--- app/screens/AddCard/index.js
+++ app/screens/AddCard/index.js
@@ -1,61 +1,81 @@
 import React from 'react';
-import {TouchableOpacity, TextInput, Text, View, StyleSheet} from 'react-native';
-import { NavigationActions } from 'react-navigation';
-import { connect } from 'react-redux';
-import { addCard } from '../../actions';
+import {
+  Alert,
+  TouchableOpacity,
+  TextInput,
+  Text,
+  View,
+  StyleSheet
+} from 'react-native';
+import {NavigationActions} from 'react-navigation';
+import {connect} from 'react-redux';
+import {addCard} from '../../actions';
 import styles from './styles';
 
 class AddCard extends React.Component {
 
   state = {
     question: '',
-    answer:   '',
+    answer: ''
   }
 
   handleQuestionChange = (question) => {
     this.setState(oldState => {
-      return {...oldState, question }
+      return {
+        ...oldState,
+        question
+      }
     })
   }
 
   handleAnswerChange = (answer) => {
     this.setState(oldState => {
-      return {...oldState, answer }
+      return {
+        ...oldState,
+        answer
+      }
     })
   }
 
   addCard = () => {
 
-    if (!this.state.question || !this.state.answer) return;
+    const q = this.state.question;
+    const a = this.state.answer;
+
+    if (!q || !a) {
+      let msg = ''
+      if (!q && !a)
+        msg = 'Question and answer are both required';
+      else if (!q)
+        msg = 'Question is required'
+      else
+        msg = 'Answer is required'
+
+      Alert.alert('Error', msg);
+
+      return;
+    }
 
     this.props.addCard(this.props.deck.key, this.state);
 
-    console.log(this.props.deck);
-
-    const setParamsAction = NavigationActions.setParams({
-      params: {deck: this.props.deck},
-      key: 'Deck'
-    });
-
-    this.props.navigation.dispatch(setParamsAction);
-    // this.props.navigation.goBack();
+    Alert.alert('Card Added', 'Successfully added a new card!', [
+      {
+        text: 'Keep Adding',
+        onPress: () => {}
+      }, {
+        text: 'I\'m Done',
+        onPress: () => {
+          this.props.navigation.goBack();
+        }
+      }
+    ]);
   }
 
   render() {
     return (<View>
-      <TextInput
-        style={styles.formInput}
-        onChangeText={this.handleQuestionChange}
-        value={this.state.question}
-        placeholder="Question"
-      />
+      <TextInput style={styles.formInput} onChangeText={this.handleQuestionChange} value={this.state.question} placeholder="Question"/>
 
-      <TextInput
-        style={styles.formInput}
-        onChangeText={this.handleAnswerChange}
-        value={this.state.answer}
-        placeholder="Answer"
-      />
+      <TextInput style={styles.formInput} onChangeText={this.handleAnswerChange} value={this.state.answer} placeholder="Answer"/>
 
       <TouchableOpacity style={styles.button} onPress={this.addCard}>
         <Text style={styles.buttonText}>Add Card</Text>
@@ -68,11 +88,7 @@ class AddCard extends React.Component {
 function stateToProps(state, ownProps) {
   const {deck, addCard} = ownProps.navigation.state.params
   const activeDeck = state.decks.find(d => d.key === deck.key);
-  return {
-    decks: state.decks,
-    deck: activeDeck,
-    cards: activeDeck.cards
-  }
+  return {decks: state.decks, deck: activeDeck, cards: activeDeck.cards}
 }
 
 function dispatchToProps(dispatch) {
