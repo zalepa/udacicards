3c2f8a1a2af785e5fa1a4c5348c051aaa6e91491
George Zalepa <george.zalepa@gmail.com>
2018-03-24 15:59:14 -0400

    Re-style PreviewCard

--- app/components/PreviewCard.js
+++ app/components/PreviewCard.js
@@ -6,11 +6,15 @@ const PreviewCard = ({deck, onPress}) => {
   return (
     <TouchableOpacity onPress={() => onPress(deck)}>
       <View style={styles.card}>
-        <Text style={styles.header}>
-          <MaterialCommunityIcons style={styles.icon} name='cards-variant' size={25}/>
-          <Text> {deck.title}</Text>
-        </Text>
-        <Text style={styles.size}>({deck.cards.length} cards)</Text>
+        <MaterialCommunityIcons style={styles.icon}
+                                name='cards-variant'
+                                size={30}/>
+        <View style={styles.header}>
+          <Text numberOfLines={1} style={styles.headerText}>{deck.title}</Text>
+        </View>
+        <View style={styles.size}>
+          <Text style={styles.sizeText}>({deck.cards.length} cards)</Text>
+        </View>
       </View>
     </TouchableOpacity>
   )
@@ -18,30 +22,40 @@ const PreviewCard = ({deck, onPress}) => {
 
 const styles = StyleSheet.create({
   card: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-around',
     borderColor: '#f0f0f0',
     borderWidth: 1,
     borderRadius: 10,
-    padding: 10,
-    paddingLeft: 10,
-    margin: 10,
+    paddingTop: 15,
+    paddingBottom: 15,
+    margin: 15,
+  },
+
+  icon: {
+    padding: 5,
+    paddingLeft: 8,
+    textAlign: 'center'
+  },
+  headerText: {
+    fontSize: 25,
   },
   header: {
-    fontSize: 25,
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  icon: {
-    width: 100
+    justifyContent: 'center',
+    paddingLeft: 5,
   },
   size: {
-    textAlign: 'center',
+    justifyContent: 'center',
+  },
+  sizeText: {
+    color: '#afafaf',
     fontSize: 15,
-    fontStyle: 'italic'
-  }
+    fontStyle: 'italic',
+    padding: 5,
+    paddingRight: 7,
+  },
 });
 
 export default PreviewCard;
